a7d46899f42968d7115fdbe0d3be90493990139e
Gilney Dell <gilneysantana@gmail.com>
2020-04-19 16:18:12 -0300

    adicionei persistencia de user. passando nos testes

---
+++ src/CleanArchitecture.Infrastructure/Data/Config/UserConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using CleanArchitecture.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchitecture.Infrastructure.Data.Config
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.Property(t => t.Name)
+                .IsRequired();
+
+            builder.OwnsOne(u => u.Credentials, a => {
+                a.WithOwner();
+
+                a.Property(a => a.Username)
+                    .HasMaxLength(30)
+                    .IsRequired();
+
+                a.Property(a => a.Password)
+                    .HasMaxLength(20)
+                    .IsRequired();
+            });
+
+            builder.OwnsOne(t => t.Email, a =>
+            {
+                a.WithOwner();
+
+                a.Property(a => a.Address)
+                    .IsRequired();
+            });
+        }
+    }
+}
